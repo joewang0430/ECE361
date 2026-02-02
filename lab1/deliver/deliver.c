3d1c32cf62b5d745d99c79c0654d97c061e27ccc
Joe Wang <146039921+joewang0430@users.noreply.github.com>
2026-02-02 15:28:59 -0500

    Add files via upload

--- lab1/deliver/deliver.c
+++ lab1/deliver/deliver.c
@@ -4,69 +4,85 @@
 #include <unistd.h>     // UNIX std funcs (close, access) 
 #include <arpa/inet.h>  // IP address convert funcs (inet_addr, htons)
 #include <time.h>       
-#include <sys/socket.h> // Socket core funcs (socket, bind, sendto, recvfrom) [cite: 97]
+#include <sys/socket.h> // Socket core funcs (socket, bind, sendto, recvfrom) 
 #include <netinet/in.h> // Internet addr family structures (sockaddr_in)
 #include <netdb.h>      // Internet db operation (gethostbyname - used for getting IP based on host name)
 
-#define MAXBUFLEN 100
+#define MAX_BUF_LEN 100
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "usage: deliver <server address> <server port number>\n");
+        fprintf(stderr, "arg mun false. correct: deliver <server address> <server port number>\n");
         exit(1);
     }
 
-    int sockfd;
-    struct sockaddr_in servaddr;
-    struct hostent *he;
-    char input[MAXBUFLEN], filename[MAXBUFLEN], cmd[MAXBUFLEN];
+    int sockfd;         // used for creating socket handler
+    struct sockaddr_in servaddr;    // used to store the IP and PORT of server
+    struct hostent *he;             // DNS result: used to store the returned information from gethostbyname funtion
+    char input[MAX_BUF_LEN], filename[MAX_BUF_LEN], cmd[MAX_BUF_LEN];
 
-    // get server IP address
-    if ((he = gethostbyname(argv[1])) == NULL) {
+    // get server IP address: convert the input domain name to the system-recognizable IP
+    if ((he = gethostbyname(argv[1])) == NULL) {    // gethostbyname: look up the DNS 
         perror("gethostbyname");
         exit(1);
     }
 
     // prompt user enter "ftp <file name>"
     printf("Enter message: ");
-    if (fgets(input, sizeof(input), stdin) == NULL) exit(1);
-    sscanf(input, "%s %s", cmd, filename);
+    if (fgets(input, sizeof(input), stdin) == NULL) exit(1);    // input: defined buffer arr; sizeof(): limit size; stdin: where to read from
+    sscanf(input, "%s %s", cmd, filename);  // stores "ftp" into cmd，stroes "myphoto.jpg" into filename
 
-    // check if file actually exists
+    // check if file actually exists: cmd must be "ftp" and file must exists
     if (strcmp(cmd, "ftp") != 0 || access(filename, F_OK) != 0) {
         printf("File not found or invalid command. Exiting.\n");
         exit(1);
     }
 
-    // create UDP socket
+    // create UDP socket, SOCK_DGRAM means UDP; AF_INET: IPV4; 0: default UDP in given previous parameterss
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("socket");
         exit(1);
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(atoi(argv[2]));
-    memcpy(&servaddr.sin_addr, he->h_addr_list[0], he->h_length);
+    memset(&servaddr, 0, sizeof(servaddr)); // clear the memory first to prevent garbage initially
+    servaddr.sin_family = AF_INET;          // confirm to use IPV4
+    servaddr.sin_port = htons(atoi(argv[2]));   // htons = Host TO Network Short; convert argv[2] from ACCII to INTEGER by atoi
+    memcpy(&servaddr.sin_addr, he->h_addr_list[0], he->h_length);   // copy the IP address from previous DNS (he->h_addr_list[0]) to servaddr
+
+    // timer， record start clock
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock(); 
 
     // send ftp to server
+    // sendto(from who, what content, how long the content, sign bit, to who, address length)
     sendto(sockfd, "ftp", 3, 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-    // receive server feedback
-    char buf[MAXBUFLEN];
-    int numbytes = recvfrom(sockfd, buf, MAXBUFLEN - 1, 0, NULL, NULL);
+    // record end clock
+    end = clock();
+
+    // wait to receive server feedback
+    // recvfrom(who receive, store where, how long, sign bit, sent from who?, sender address length?)
+    char buf[MAX_BUF_LEN];
+    int numbytes = recvfrom(sockfd, buf, MAX_BUF_LEN - 1, 0, NULL, NULL);
     if (numbytes < 0) {
         perror("recvfrom failed");
         exit(1);
     }
-    buf[numbytes] = '\0';
+    buf[numbytes] = '\0';   // mannually add \0 at the end to prevent random code
 
+    // calculate RTT
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("RTT = %f seconds\n", cpu_time_used);
+
+    // check if server replied yes
     if (strcmp(buf, "yes") == 0) {
-        printf("A file transfer can start.\n"); 
+        printf("Received yes. A file transfer can start.\n"); 
     } else {
         exit(1); 
     }
 
+    // end socket
     close(sockfd);
     return 0;
 }
--- lab1/server/server.c
+++ lab1/server/server.c
@@ -4,21 +4,22 @@
 #include <unistd.h>     // UNIX std funcs (close, access) 
 #include <arpa/inet.h>  // IP address convert funcs (inet_addr, htons)
 #include <time.h>       
-#include <sys/socket.h> // Socket core funcs (socket, bind, sendto, recvfrom) [cite: 97]
+#include <sys/socket.h> // Socket core funcs (socket, bind, sendto, recvfrom) 
 #include <netinet/in.h> // Internet addr family structures (sockaddr_in)
 #include <netdb.h>      // Internet db operation (gethostbyname - used for getting IP based on host name)
 
-#define MAXBUFLEN 100
+#define MAX_BUF_LEN 100
 
 int main(int argc, char *argv[]) {
+    // check the parameter
     if (argc != 2) {
         fprintf(stderr, "arg number fault\n");
         exit(1);
     }
 
     int sockfd;
-    struct sockaddr_in servaddr, cliaddr;
-    char buf[MAXBUFLEN];
+    struct sockaddr_in servaddr, cliaddr;   // client address： to get the address from client as don't know at the beginning
+    char buf[MAX_BUF_LEN];
     socklen_t addr_len;
 
     // create UDP socket
@@ -27,13 +28,15 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // fill in the address information of server
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(atoi(argv[1])); // convert port byte order
+    servaddr.sin_addr.s_addr = INADDR_ANY;  // receive as long as the port is currect, no matter network card
+    servaddr.sin_port = htons(atoi(argv[1])); // convert port byte order, Host to Network Short
 
+    // bind the socket to the IP just set
     if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        perror("failed biding");
+        perror("failed binding");
         close(sockfd);
         exit(1);
     }
@@ -42,13 +45,17 @@ int main(int argc, char *argv[]) {
 
     while (1) {
         addr_len = sizeof(cliaddr);
-        // receive the server message
-        int numbytes = recvfrom(sockfd, buf, MAXBUFLEN - 1, 0, (struct sockaddr *)&cliaddr, &addr_len);
+        // receive the server message, blocking receive
+        // recvfrom(handle, buffer, length, 0, client address, length pointer)
+        int numbytes = recvfrom(sockfd, buf, MAX_BUF_LEN - 1, 0, (struct sockaddr *)&cliaddr, &addr_len);
+
+        // if receive has error, just continue to the next loop
         if (numbytes < 0) continue;
 
         buf[numbytes] = '\0';
 
         // reply based on the msg content
+        // see whether is begin with "ftp"
         if (strcmp(buf, "ftp") == 0) {
             sendto(sockfd, "yes", 3, 0, (struct sockaddr *)&cliaddr, addr_len);
         } else {
